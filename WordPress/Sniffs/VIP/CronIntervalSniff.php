37593b754ff346d96951df69092c3120aa729c80
jrfnl <github_nospam@adviesenzo.nl>
2017-02-01 16:37:35 +0100

    Add `process()` method to WordPress_Sniff and change to `process_token()` method in child classes.
    This saves us having to call `$this->init()` from every child class, always sets the basic properties and makes them available to the child classes.

--- WordPress/Sniffs/VIP/CronIntervalSniff.php
+++ WordPress/Sniffs/VIP/CronIntervalSniff.php
@@ -35,15 +35,14 @@ class WordPress_Sniffs_VIP_CronIntervalSniff extends WordPress_Sniff {
 	/**
 	 * Processes this test, when one of its tokens is encountered.
 	 *
-	 * @param PHP_CodeSniffer_File $phpcsFile The file being scanned.
-	 * @param int                  $stackPtr  The position of the current token
-	 *                                        in the stack passed in $tokens.
+	 * @param int $stackPtr The position of the current token in the stack.
 	 *
 	 * @return void
 	 */
-	public function process( PHP_CodeSniffer_File $phpcsFile, $stackPtr ) {
+	public function process_token( $stackPtr ) {
+		$phpcsFile = $this->phpcsFile;
 		$tokens = $phpcsFile->getTokens();
-		$token  = $tokens[ $stackPtr ];
+		$token  = $this->tokens[ $stackPtr ];
 
 		if ( 'cron_schedules' !== $this->strip_quotes( $token['content'] ) ) {
 			return;
